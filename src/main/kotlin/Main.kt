180d850112e4a14c1bbb1ffb3331495c17a51d4b
Angelina-2022 <batinaa6@gmail.com>
2023-09-09 17:46:19 +0300

    pr1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,46 @@
+fun main(args: Array<String>) {
+//задача 1
+    val exercises: Int = 13
+    var exercisesSolved: Int = 0 //увеличивается каждый раз при решении задачи на 1(начиная с этой)
+    exercisesSolved ++
+    //задача2
+    val myAge: Int = 19
+    exercisesSolved ++
+    //задача3
+    var personAge: Double = 19.0
+    personAge = (personAge + 30.0)/2
+    exercisesSolved ++
+    //задача4
+    val testNumber: Int = 18
+    val evenOdd= testNumber%2
+    /* если изменить значение testNumber на четное число, то значение EvenOdd =0, если на нечетное, то 1*/
+    exercisesSolved ++
+    //задача5
+    var answer: Int = 0
+    answer ++
+    answer +=  10
+    answer *=10
+    answer= answer shr 3
+    /* println(answer)
+     при выводе в консоль получаем значение 13 */
+    exercisesSolved ++
+    //задача6
+    var age : Int
+    /* use var типа int потому что в дальнейшем значение будет меняться*/
+    exercisesSolved ++
+    //задача7
+    val a1: Int=46
+    val b1: Int = 10
+    val answer1: Int = (a1*100)+b1            //println(answer1) =4610
+    val answer2:Int = (a1*100)+(b1*100)  //println(answer2) =5600
+    val answer3:Int = (a1*100)+(b1/10)    //println(answer3) =4601
+    exercisesSolved ++
+    //задача8
+    (5*3)-((4/2)*2)
+    exercisesSolved ++
+    //задача9
+    val a: Double=17.0
+    val b: Double=29.0
+    val average =(a+b)/2
+    exercisesSolved ++
+}
